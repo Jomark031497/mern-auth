898959c72b42d87d529fd8726f39057f54973f4d
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-29 10:35:25 +0800

    Added comments

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middlewares
 app.use(express.json());
+app.use(cors());
 
+// connect to mongoDB
 mongoose.connect(
   process.env.MONGODB_URI,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
@@ -16,8 +20,10 @@ mongoose.connect(
   }
 );
 
+// routes
 app.use("/auth", require("./routers/userRoutes"));
 
+// connect to server
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
