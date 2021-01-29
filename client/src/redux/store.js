c59e63ccbac3e22704dc0b95956b8a00f47dcecf
Jomark Pangan <jomark@quantumcrowd.io>
2021-01-29 13:21:23 +0800

    Added redux store

---
+++ client/src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+  // TODO
+});
+
+let initialState = {};
+
+const middlewares = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export default store;
